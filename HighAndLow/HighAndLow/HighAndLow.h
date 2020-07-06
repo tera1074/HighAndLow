c25d7f9b2d6f09079cdce9aab8ce1d217a17d47a
tera1074 <jagapotera.0814t@ezweb.ne.jp>
2020-07-07 03:02:08 +0900

    ゲーム部分追加

--- HighAndLow/HighAndLow/HighAndLow.h
+++ HighAndLow/HighAndLow/HighAndLow.h
@@ -6,4 +6,6 @@ void getCurrentDirectory(char *currentDirectory);
 bool writeChar(const char *section, const char *keyword, const char *returnValue, const char *filePath);
 bool writeInt(const char *section, const char *keyword, int returnValue, const char *filePath);
 bool writeDouble(const char *section, const char *keyword, double returnValue, const char *filePath);
+int getRand(int x);
+void initRand();
 
